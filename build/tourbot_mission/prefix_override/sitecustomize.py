d30c8b86273def9e623ed3a31469f303845ca44a
Hyaxon <108911977+Hyaxon@users.noreply.github.com>
2026-04-22 23:07:43 -0500

    Removed cache and fixed commands

--- build/tourbot_mission/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/hyde/School/Intelligent-Robotics/tour_guide_robot/install/tourbot_mission'
