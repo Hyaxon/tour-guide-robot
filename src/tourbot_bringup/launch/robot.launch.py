49f08e3010ede36eb249ce540414e866a4543807
Hyaxon <108911977+Hyaxon@users.noreply.github.com>
2026-04-29 18:37:08 -0500

    Launch test

--- src/tourbot_bringup/launch/robot.launch.py
+++ src/tourbot_bringup/launch/robot.launch.py
@@ -1,11 +1,13 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, LogInfo
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 
 def generate_launch_description():
+    # Path to predefined map
     map_yaml = os.path.join(
         get_package_share_directory('tourbot_bringup'),
         'maps',
@@ -13,22 +15,44 @@ def generate_launch_description():
         'map_area.yaml'
     )
 
-    nav_bringup_launch = IncludeLaunchDescription(
+    # Launch rviz2 in navigation mode
+    view_navigation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('turtlebot4_viz'),
+                'launch',
+                'view_navigation.launch.py'
+            )
+        )
+    )
+
+    # Localize TurtleBot4 using predefined map instead of running SLAM
+    localization_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
                 get_package_share_directory('turtlebot4_navigation'),
                 'launch',
-                'nav_bringup.launch.py'
+                'localization.launch.py'
             )
         ),
         launch_arguments={
-            'slam': 'off',
-            'localization': 'true',
             'map': map_yaml,
         }.items()
     )
 
+    nav2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('turtlebot4_navigation'),
+                'launch',
+                'nav2.launch.py'
+            )
+        )
+    )
+    
+
     return LaunchDescription([
-        LogInfo(msg=['Using map: ', map_yaml]),
-        nav_bringup_launch,
+        view_navigation_launch,
+        #localization_launch,
+        nav2_launch
     ])
