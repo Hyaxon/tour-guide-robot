6619fb8dc1410de031a82c77a859d035ef078f97
Hyaxon <108911977+Hyaxon@users.noreply.github.com>
2026-04-29 17:55:26 -0500

    Robot launch

---
+++ src/tourbot_bringup/launch/robot.launch.py
@@ -0,0 +1,55 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    # Path to predefined map
+    map_yaml = os.path.join(
+        get_package_share_directory('tourbot_bringup'),
+        'maps',
+        'cardboard_city',
+        'map_area.yaml'
+    )
+
+    # Launch rviz2 in navigation mode
+    view_navigation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('turtlebot4_viz'),
+                'launch',
+                'view_navigation.launch.py'
+            )
+        )
+    )
+
+    # Localize TurtleBot4 using predefined map instead of running SLAM
+    localization_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('turtlebot4_navigation'),
+                'launch',
+                'localization.launch.py'
+            )
+        ),
+        launch_arguments={
+            'map': map_yaml,
+        }.items()
+    )
+
+    # Launches the controller node
+    controller_node = Node(
+        package='project2_control',
+        executable='project2_controller',
+        name='project2_controller',
+        output='screen',
+    )
+
+    return LaunchDescription([
+        controller_node,
+        view_navigation_launch,
+        localization_launch
+    ])
