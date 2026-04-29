5e043d4d0eb4f38bf772aef4f8e0c9dcf0a97194
Hyaxon <108911977+Hyaxon@users.noreply.github.com>
2026-04-29 14:22:58 -0500

    Custom world sim launch

--- src/tourbot_bringup/launch/sim.launch.py
+++ src/tourbot_bringup/launch/sim.launch.py
@@ -1,22 +1,30 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
-use_custom_sim = False
 
 def generate_launch_description():
     turtlebot4_gz_bringup = FindPackageShare('turtlebot4_gz_bringup')
+    tourbot_bringup = FindPackageShare('tourbot_bringup')
+
+    use_custom_sim = LaunchConfiguration('use_custom_sim')
+    custom_world = LaunchConfiguration('custom_world')
+    custom_map = LaunchConfiguration('custom_map')
+
+    turtlebot4_launch = PythonLaunchDescriptionSource(
+        PathJoinSubstitution([
+            turtlebot4_gz_bringup,
+            'launch',
+            'turtlebot4_gz.launch.py',
+        ])
+    )
 
     default_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                turtlebot4_gz_bringup,
-                'launch',
-                'turtlebot4_gz.launch.py',
-            ])
-        ),
+        turtlebot4_launch,
+        condition=UnlessCondition(use_custom_sim),
         launch_arguments={
             'nav2': 'true',
             'slam': 'false',
@@ -26,24 +34,48 @@ def generate_launch_description():
     )
 
     custom_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                turtlebot4_gz_bringup,
-                'launch',
-                'turtlebot4_gz.launch.py',
-            ])
-        ),
+        turtlebot4_launch,
+        condition=IfCondition(use_custom_sim),
         launch_arguments={
             'nav2': 'true',
             'slam': 'false',
             'localization': 'true',
             'rviz': 'true',
+
+            'world': custom_world,
+            'map': custom_map,
         }.items(),
     )
 
-
-    sim_launch = default_sim if not use_custom_sim else custom_sim
-
     return LaunchDescription([
-        sim_launch,
+        DeclareLaunchArgument(
+            'use_custom_sim',
+            default_value='false',
+            description='Launch with custom tourbot world and map',
+        ),
+
+        DeclareLaunchArgument(
+            'custom_world',
+            default_value=PathJoinSubstitution([
+                tourbot_bringup,
+                'worlds',
+                'cardboard_city',
+                'cardboard_city.sdf',
+            ]),
+            description='Custom Gazebo world name or path',
+        ),
+
+        DeclareLaunchArgument(
+            'custom_map',
+            default_value=PathJoinSubstitution([
+                tourbot_bringup,
+                'maps',
+                'cardboard_city',
+                'map_area.yaml',
+            ]),
+            description='Custom Nav2 map YAML path',
+        ),
+
+        default_sim,
+        custom_sim,
     ])
