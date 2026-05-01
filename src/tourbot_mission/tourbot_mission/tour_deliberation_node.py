c194fb57de96193f2019254aae77a8dd3efe9bac
Hyde <108911977+Hyaxon@users.noreply.github.com>
2026-05-01 11:41:56 -0500

    Added doors template to landmarks, apriltag ids, and sequential nav

--- src/tourbot_mission/tourbot_mission/tour_deliberation_node.py
+++ src/tourbot_mission/tourbot_mission/tour_deliberation_node.py
@@ -20,6 +20,21 @@ def landmark_to_pose(navigator: TurtleBot4Navigator, landmark: dict):
         to_direction(landmark["theta"])
     )
 
+
+def get_nearest_landmark(l1x, l1y, landmark_raw_data):
+    nearest_landmark = 0
+    nearest_dist = float("inf")
+
+    for i, landmark in enumerate(landmark_raw_data):
+        dist = (l1x - landmark["x"]) ** 2 + (l1y - landmark["y"]) ** 2
+
+        if dist < nearest_dist:
+            nearest_landmark = i
+            nearest_dist = dist
+
+    return nearest_landmark
+
+
 def main():
     rclpy.init()
 
@@ -42,15 +57,38 @@ def main():
     navigator.waitUntilNav2Active()
 
     # Set goal poses
-    goal_poses = []
+    landmark_raw_data = []
     for landmark in landmark_data["landmarks"]:
-        goal_poses.append(landmark_to_pose(navigator, landmark))
+        landmark_raw_data.append(landmark)
+
+    goal_poses = []
+    last_landmark = landmark_data['home']
+    while landmark_raw_data:
+        nearest_landmark = get_nearest_landmark(
+            last_landmark['x'], 
+            last_landmark['y'], 
+            landmark_raw_data
+        )
+
+        goal_poses.append(landmark_to_pose(navigator, landmark_raw_data[nearest_landmark]))
+        last_landmark = landmark_raw_data.pop(nearest_landmark)
+
+    # Return home to end the tour
+    goal_poses.append(initial_pose)
+
 
     # Undock
     #navigator.undock()
 
     # Go to each goal pose
-    navigator.startThroughPoses(goal_poses)
+    for goal_pose in goal_poses:
+        navigator.startToPose(goal_pose)
+
+        while not navigator.isTaskComplete():
+            rclpy.spin_once(navigator, timeout_sec=0.1)
+
+        navigator.info("Reached landmark")
+
 
     #navigator.dock()
 
