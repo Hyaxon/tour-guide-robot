8109a7a53fccafb500a0c13aebaad5a7daa8de53
Hyde <108911977+Hyaxon@users.noreply.github.com>
2026-05-01 19:44:36 -0500

    new nav2 config

--- src/tourbot_bringup/launch/robot.launch.py
+++ src/tourbot_bringup/launch/robot.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
     nav2_params = os.path.join(
         get_package_share_directory('tourbot_bringup'),
         'config',
-        'nav2_params_old.yaml'
+        'nav2_test_help.yaml'
     )
 
     # Launch rviz2 in navigation mode
